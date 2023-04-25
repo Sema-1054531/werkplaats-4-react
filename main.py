6286d01d2490ac41daa67641f81ce608ca410f09
Sema-1054531 <1054531@hr.nl>
2023-04-25 21:31:01 +0200

    added: questions

--- main.py
+++ main.py
@@ -1,2 +1,46 @@
-import flask, pi, js, react, express
+from flask import Flask, render_template, g
 
+import sqlite3
+import os
+
+app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'lib/werkplaats4.db')
+
+LISTEN_ALL = "0.0.0.0"
+FLASK_IP = LISTEN_ALL
+FLASK_PORT = 82
+FLASK_DEBUG = True
+
+def get_db():
+    """Opens a new database connection if there is none  yet for the current application context."""
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(app.config['DATABASE'])
+        db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+conn = sqlite3.connect('lib/werkplaats4.db')
+
+@app.route('/')
+def index():
+    return "hallo"
+
+
+@app.route('/questions')
+def show_questions():
+    db = get_db()
+    cur = db.cursor()
+    cur.execute('SELECT * FROM question')
+    questions = cur.fetchall()
+    return render_template('questions.html', questions=questions)
+
+if __name__ == "__main__":
+    app.run(host=FLASK_IP, port=FLASK_PORT, debug=FLASK_DEBUG)
