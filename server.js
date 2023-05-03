ef94a185091ec18c202bd01c1ff1a06743c107d7
Sema-1054531 <1054531@hr.nl>
2023-05-03 23:55:30 +0200

    added: server.js (make new question)

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
+
+const app = express();
+
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// create connection to database
+const db = new sqlite3.Database('lib/werkplaats4.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the database.');
+});
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// create a new question
+app.post('/questions/new', (req, res) => {
+    const { question_text, question_type, is_active } = req.body;
+    const sql = 'INSERT INTO question (question_text, question_type, is_active) VALUES (?, ?, ?)';
+    db.run(sql, [question_text, question_type, is_active], function (err) {
+        if (err) {
+            console.log(err.message);
+            res.status(500).send('Error creating question');
+        } else {
+            console.log(`Question added with ID: ${this.lastID}`);
+            res.send(`Question created with ID: ${this.lastID}`);
+        }
+    });
+});
+
+// get form to create new question
+app.get('/questions/new', (req, res) => {
+    res.sendFile(__dirname + '/templates/add_questions.html');
+});
+
+// get all questions
+app.get('/questions', (req, res) => {
+    const sql = 'SELECT * FROM question';
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Error getting questions');
+        } else {
+            res.send(rows);
+        }
+    });
+});
+
+// start server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
