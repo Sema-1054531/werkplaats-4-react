035dd5d6797169d7b57732bbbbd2a6bcb82939a1
Kenan-1035868 <1035868@hr.nl>
2023-05-30 20:03:14 +0200

    CHANGES: LOGIN AND REGISTER FUNCTION ADDED

--- one_piece/server/server.js
+++ one_piece/server/server.js
@@ -14,6 +14,50 @@ app.use(express.json());
 
 app.use('/', routes);
 
+// Endpoint voor het registreren van een gebruiker
+app.post('/api/register', (req, res) => {
+  const { username, password } = req.body;
+
+  // Controleer of de gebruiker al bestaat in de database
+  db.query('SELECT * FROM users WHERE username = ?', username, (err, results) => {
+    if (err) {
+      res.status(500).json({ message: 'Registration failed' });
+    } else {
+      if (results.length > 0) {
+        res.status(409).json({ message: 'User already exists' });
+      } else {
+        // Voeg de nieuwe gebruiker toe aan de database
+        db.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (err, result) => {
+          if (err) {
+            res.status(500).json({ message: 'Registration failed' });
+          } else {
+            res.json({ message: 'Registration successful' });
+          }
+        });
+      }
+    }
+  });
+});
+
+// Endpoint voor het inloggen van een gebruiker
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Zoek de gebruiker in de database
+  db.query('SELECT * FROM users WHERE username = ?', username, (err, results) => {
+    if (err) {
+      res.status(500).json({ message: 'Login failed' });
+    } else {
+      if (results.length > 0 && results[0].password === password) {
+        res.json({ message: 'Login successful' });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    }
+  });
+});
+
+
 app.post('/surveys/new', (req, res) => {
   // Verwerk het POST-verzoek naar /surveys
     const { survey_title, survey_description, is_anonymous } = req.body;
--- one_piece/src/App.js
+++ one_piece/src/App.js
@@ -1,21 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 import AddSurvey from "./components/AddSurvey";
 import Surveys from "./components/Surveys";
 import AddQuestionToSurvey from "./components/AddQuestionToSurvey";
 
 const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleRegister = () => {
+    axios.post('/api/register', { username, password })
+      .then(response => setMessage(response.data.message))
+      .catch(error => setMessage('Registration failed'));
+  };
+
+  const handleLogin = () => {
+    axios.post('/api/login', { username, password })
+      .then(response => setMessage(response.data.message))
+      .catch(error => setMessage('Login failed'));
+  };
+
+  const Login = () => {
+    return (
+      <div>
+        <h2>Login</h2>
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username" />
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
+        <button onClick={handleLogin}>Login</button>
+        <p>{message}</p>
+      </div>
+    );
+  };
+
+  const Register = () => {
+    return (
+      <div>
+        <h2>Register</h2>
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username" />
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
+        <button onClick={handleRegister}>Register</button>
+        <p>{message}</p>
+      </div>
+    );
+  };
+
+  
+
   return (
     <Router>
-        <div>
-            <Routes>
-                <Route path="/surveys" element={<Surveys />} />
-                <Route path="/surveys/new" element={<AddSurvey />} />
-                <Route path="/surveys/:survey_id/add_question" element={<AddQuestionToSurvey />} />
-                {/* Andere routes */}
-            </Routes>
-        </div>
+      <div>
+        <Routes>
+          <Route path="/surveys" element={<Surveys />} />
+          <Route path="/surveys/new" element={<AddSurvey />} />
+          <Route path="/surveys/:survey_id/add_question" element={<AddQuestionToSurvey />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* Andere routes */}
+        </Routes>
+      </div>
     </Router>
   );
 };
