52ac54246a5d63bf0559e491859906b88aa3d576
dennis-1031879 <1031879@hr.nl>
2023-05-31 17:36:50 +0200

    Update: Implemented the delete survey prompt to be consistent with other prompts.

--- one_piece/src/components/DeleteSurvey.js
+++ one_piece/src/components/DeleteSurvey.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link, useLocation} from "react-router-dom";
+import DeleteSurveyForm from "./DeleteSurveyForm";
 
 const Header = () => {
     return (
@@ -9,25 +9,20 @@ const Header = () => {
     );
 };
 
+
 const DeleteSurvey = () => {
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-    const survey_title = searchParams.get("title");
-
-
-    const deletingSurvey = () => {
-
-    };
-
     return (
-    <div className="container">
-        <h4 className="my-4">Wil je de enquêtte <b>{survey_title}</b> verwijderen?</h4>
-        <button className="btn btn-primary" onClick={deletingSurvey}>Verwijder</button>
-        <Link to={"/surveys"} className="btn btn-secondary">Ga terug</Link>
-
-    </div>
+        <div className="container">
+            <Header />
+            <main>
+                <div className="row">
+                    <div className="col">
+                        <DeleteSurveyForm />
+                    </div>
+                </div>
+            </main>
+        </div>
     );
-
 };
 
 export default DeleteSurvey;
---
+++ one_piece/src/components/DeleteSurveyForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link, useLocation} from "react-router-dom";
+
+
+const DeleteSurveyForm = () => {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const survey_title = searchParams.get("title");
+
+
+    const deletingSurvey = () => {
+
+    };
+
+    return (
+    <div className="container">
+        <h4 className="my-4">Wil je de enquêtte <b>{survey_title}</b> verwijderen?</h4>
+        <button className="btn btn-primary" onClick={deletingSurvey}>Verwijder</button>
+        <Link to={"/surveys"} className="btn btn-secondary">Ga terug</Link>
+
+    </div>
+    );
+
+};
+
+export default DeleteSurveyForm;
