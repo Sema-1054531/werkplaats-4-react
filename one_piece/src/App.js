d891704d7fac9b9749f9c20f15d723a5e676b45f
romius-1059571 <romiusboy@hotmail.com>
2023-05-31 16:53:33 +0200

    All Questions ADDED

--- one_piece/src/App.js
+++ one_piece/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddQuestionForm from "./components/AddQuestionForm";
+import AllQuestions from "./components/AllQuestions";
 
 const App = () => {
   return (
     <Router>
         <div>
             <Routes>
-                <Route path="/questions" element={<AddQuestionForm />} />
+                <Route path="/questions/new" element={<AddQuestionForm />} />
+                <Route path="/questions" element={<AllQuestions />} />
                 {/* Andere routes */}
             </Routes>
         </div>
---
+++ one_piece/src/components/AllQuestions.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const AllQuestions = () => {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
+  const fetchQuestions = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/questions');
+      setQuestions(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <table className="table table-striped">
+        <thead className="thead-dark">
+          <tr>
+            <th>Vragen</th>
+            <th>Soort Vraag</th>
+            <th>Is Actief</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {questions.map(question => (
+            <tr key={question.question_id}>
+              <td>{question.question_text}</td>
+              <td>{question.question_type}</td>
+              <td>{question.is_active ? "Open" : "Meerkeuze"}</td>
+              <td>
+                <Link to={`/questions/${question.question_id}/wijzig?title=${encodeURIComponent(question.question_text)}`}>Wijzig</Link>
+              </td>
+              <td>
+                <Link to="#">Delete</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default AllQuestions;
