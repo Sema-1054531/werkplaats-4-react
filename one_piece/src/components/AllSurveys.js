051d650b45c93985f4c1ac72ceec5d9583fb1f55
Sema-1054531 <1054531@hr.nl>
2023-06-06 15:18:59 +0200

    Added: All surveys that are done

---
+++ one_piece/src/components/AllSurveys.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState} from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
+
+const AllSurveys = () => {
+    const [surveys, setSurveys] =useState([]);
+
+    useEffect(() => {
+        fetchSurveys();
+    }, []);
+
+    const fetchSurveys = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/surveys');
+            setSurveys(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    // only show surveys that are not done
+    const filteredSurveys = surveys.filter((survey) => survey.is_done === 1);
+
+    return(
+        <div>
+            <table className="table table-striped">
+                <thead className="thead-dark">
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                </tr>
+                </thead>
+                <tbody>
+                {filteredSurveys.map((survey) => (
+                    <tr key={survey.survey_id}>
+                        <td>{survey.survey_title}</td>
+                        <td>{survey.survey_description}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default AllSurveys;
