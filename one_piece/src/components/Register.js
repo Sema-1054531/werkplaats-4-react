949bd83966fc7abfaee8b0da8c77d442905ab92a
Sema-1054531 <1054531@hr.nl>
2023-06-08 16:23:12 +0200

    updated: layout

--- one_piece/src/components/Register.js
+++ one_piece/src/components/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import {Link} from "react-router-dom";
 
 const Register = () => {
   const [email, setEmail] = useState('');
@@ -35,40 +36,51 @@ const Register = () => {
   };
 
   return (
-    <div className="container">
-      <h2>Registratie</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email:</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+      <div className="row">
+        <div className="offset-lg-3 col-lg-6">
+          <form onSubmit={handleSubmit} className="container">
+            <div className="card">
+              <div className="card-header">
+                <h2>Meld je aan</h2>
+              </div>
+              <div className="card-body">
+                <div className="mb-3">
+                  <label>E-mail <span className="errmsg">*</span></label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label>Wachtwoord <span className="errmsg">*</span></label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <input
+                    type="hidden"
+                    value={is_admin}
+                    onChange={(e) => set_is_admin(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="card-footer">
+                <p>Heb je al een account? <Link to={'/'}>Log dan hier in</Link></p>
+                <button type="submit" className="btn btn-primary">Registreer</button>
+                {message && <p>{message}</p>}
+              </div>
+            </div>
+          </form>
         </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Wachtwoord:</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <input
-            type="hidden"
-            value={is_admin}
-            onChange={(e) => set_is_admin(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">Registreer</button>
-      </form>
-      {message && <p>{message}</p>}
-    </div>
+      </div>
   );
 };
 
